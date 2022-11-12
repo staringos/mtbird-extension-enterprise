1c4f2695f2c9d1a95086269c9fc4aefe5b3fb8e9
georgezouq <zousongqi@foxmail.com>
2022-11-12 19:31:32 +0800

    feat: add submit button in form tab

--- src/features/FormTab/components/FormComponents/index.tsx
+++ src/features/FormTab/components/FormComponents/index.tsx
@@ -1,8 +1,10 @@
-import { IComponentCommon, IExtensionContext } from '@mtbird/shared/dist/types';
+import { IComponentCommon, IExtensionContext } from '@mtbird/shared';
+import {Button} from 'antd';
 import React from 'react';
 import values from 'lodash/values';
 import ToolBoxForm from '../ToolBoxForm';
 import styles from './style.module.less';
+import { SchemaGenerator } from '@mtbird/core';
 
 interface IProps {
   context: IExtensionContext;
@@ -10,8 +12,15 @@ interface IProps {
 
 const FormComponents = ({context}: IProps) => {
   const formComponents = values(context.registeredComponents).filter((component: IComponentCommon) => component.category === 'form' && !component.hideInToolbar)
-  return <div className={styles.formTemplates}>
-    {formComponents.map((cur: IComponentCommon) => <ToolBoxForm key={cur.componentName} component={cur} context={context} />)}
+  const handleAddSubmitButton = () => {
+    context.addComponent(SchemaGenerator.formItem('', '', [SchemaGenerator.button('提交', {type: 'submit'}, {width: 100}) as any], {}))
+  }
+
+  return <div>
+    <Button type="default" block size="small" style={{marginBottom: '5px'}} onClick={handleAddSubmitButton}>提交按钮</Button>
+    <div className={styles.formTemplates}>
+      {formComponents.map((cur: IComponentCommon) => <ToolBoxForm key={cur.componentName} component={cur} context={context} />)}
+    </div>
   </div>;
 };
 
