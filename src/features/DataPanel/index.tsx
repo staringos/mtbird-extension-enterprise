d16bb3413977eebd39e90c6e768438a35e5035ac
georgezouq <zousongqi@foxmail.com>
2022-12-13 12:06:20 +0800

    feat: add data model manager btn

--- src/features/DataPanel/index.tsx
+++ src/features/DataPanel/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Alert} from 'antd'
+import {Alert, Button, message} from 'antd'
 import styles from './style.module.less'
 import { IExtensionFeatureProps } from '@mtbird/shared'
 import {COMPONENT_NAME, COMPONENT_TYPE} from '@mtbird/core'
@@ -9,7 +9,7 @@ import FieldPanel from './FieldPanel'
 const Notice = <Alert message="请选择数据容器（表格、数据列表）或其子元素进行数据绑定, 不可以多选哦～" type="warning" showIcon closable />
 
 const DataPanel = ({context}: IExtensionFeatureProps) => {
-  const {currentComponent, currentDataContainer} = context;
+  const {currentComponent, currentDataContainer, refreshDataModel} = context;
   if (!currentComponent || currentComponent.length !== 1) {
     return Notice
   }
@@ -23,10 +23,25 @@ const DataPanel = ({context}: IExtensionFeatureProps) => {
   // const dataContainerNode = getNodeFromTreeBranch(currentFirstComponent, componentMap, (cp: IComponentInstance) => cp.componentName === COMPONENT_NAME.DATA_LIST)
   const isDataChild = !!currentDataContainer
 
-  if (!currentDataContainer || !currentDataContainer.data) return Notice; 
+  const handleRefresh = async () => {
+    message.warn("刷新中...")
+    location.reload();
+    // await refreshDataModel()
+    // message.success("刷新数据模型成功")
+  }
 
   return (
     <div className={styles.dataPanelContainer}>
+      {(!currentDataContainer || !currentDataContainer.data) && Notice}
+      <div className={styles.buttonArea}>
+        <Button type="link" onClick={() => window.open('/dataCenter/dataTable')} icon={<i className="mtbird-icon mtbird-edit-square" size="small" />}>
+          管理数据
+        </Button>
+
+        <Button type="link" onClick={handleRefresh} size="small" icon={<i className="mtbird-icon mtbird-sync" />}>
+          刷新
+        </Button>
+      </div>
       {isDataContainer && (
         <DataSourcePanel context={context} currentFirstComponent={currentFirstComponent} />
       )}
